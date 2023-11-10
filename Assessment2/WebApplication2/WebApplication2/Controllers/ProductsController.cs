a6add271b1556660541b9d756ec0d08ce2784d8b
Renjie <2186464404@qq.com>
2023-11-10 20:27:45 +0800

    Assessment

---
+++ Assessment2/WebApplication2/WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,180 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private WebApplication2Context db = new WebApplication2Context();
+
+        // GET: Products
+        public ActionResult Index(string category, string search, string sortBy, int? page)
+        {
+            ProductIndexViewModel viewModel = new ProductIndexViewModel();
+            var products = db.Products.Include(p => p.Category);
+            if (!String.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name.Contains(search) ||
+                p.Description.Contains(search) ||
+                p.Category.Name.Contains(search));
+                ; viewModel.Search = search;
+            }
+            viewModel.CatsWithCount = from matchingProducts in products
+                                      where
+                                      matchingProducts.CategoryID != null
+                                      group matchingProducts by
+                                      matchingProducts.Category.Name into
+                                      catGroup
+                                      select new CategoryWithCount()
+                                      {
+                                          CategoryName = catGroup.Key,
+                                          ProductCount = catGroup.Count()
+                                      };
+            if (!String.IsNullOrEmpty(category))
+            {
+                products = products.Where(p => p.Category.Name == category);
+            }
+            switch (sortBy)
+            {
+                case "price_lowest":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                case "price_highest":
+                    products = products.OrderByDescending(p => p.Price);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Name);
+                    break;
+            }
+            viewModel.SortBy = sortBy;
+            viewModel.Sorts = new Dictionary<string, string>
+ {
+ {"Price low to high", "price_lowest" },
+ {"Price high to low", "price_highest" }
+ };
+            const int PageItems = 6;
+            int currentPage = (page ?? 1);
+            viewModel.Products = products.ToPagedList(currentPage, PageItems);
+            return View(viewModel);
+
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性；有关
+        // 更多详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Price,Description,CategoryID")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", products.CategoryID);
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", products.CategoryID);
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性；有关
+        // 更多详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Price,Description,CategoryID")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", products.CategoryID);
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products products = db.Products.Find(id);
+            db.Products.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Assessment2/WebApplication2/WebApplication2/Migrations/Configuration.cs
@@ -0,0 +1,51 @@
+﻿namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebApplication2.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication2.Data.WebApplication2Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "WebApplication2.Data.WebApplication2Context";
+        }
+
+        protected override void Seed(WebApplication2.Data.WebApplication2Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            var categories = new List<Category>
+ {
+new Category {Name = "Lenovo" },
+new Category {Name = "Xiaomi" }
+            };
+            categories.ForEach(c => context.Categories.AddOrUpdate(p => p.Name, c));
+            context.SaveChanges();
+            var products = new List<Products>
+            {
+               
+new Products {Name = "XPro162023", Price = 5699,Description = "240HZ High refresh rate", CategoryID= categories.Single(c=>c.Name == "Lenovo").ID},
+new Products {Name = "Redmibook14", Price = 3499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "Redmibook15", Price = 3499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "Redmibook16", Price = 3499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "X60pro", Price = 3499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "X70pro", Price = 4499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "X80pro", Price = 5499,Description = "High performance, high fluency",  CategoryID= categories.Single(c=>c.Name == "Xiaomi").ID},
+new Products {Name = "XPro162023", Price = 5699,Description = "240HZ High refresh rate", CategoryID= categories.Single(c=>c.Name == "Lenovo").ID},
+new Products {Name = "XPro162024", Price = 6699,Description = "240HZ High refresh rate", CategoryID= categories.Single(c=>c.Name == "Lenovo").ID},
+new Products {Name = "XPro162025", Price = 7699,Description = "240HZ High refresh rate", CategoryID= categories.Single(c=>c.Name == "Lenovo").ID},
+new Products {Name = "XPro162026", Price = 8699,Description = "240HZ High refresh rate", CategoryID= categories.Single(c=>c.Name == "Lenovo").ID}
+            };
+            products.ForEach(c => context.Products.AddOrUpdate(p => p.Name, c));
+            context.SaveChanges();
+        }
+    }
+    }
+
